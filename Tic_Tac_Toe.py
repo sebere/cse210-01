a0ad83b82fd75f5ae742f5fa6589832b9f2c8b9f
sebere <sebere0808@gmail.com>
2022-01-20 15:55:45 -0500

    last

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,92 +1,83 @@
+'''
+Tic-Tac-Toe: Solution
+Author: Sergio Benavides Reyes
+'''
 
 board = [
     "1","2","3",
     "4","5","6",
     "7","8","9"
 ]
+
+playerone = "X"
 champ = None
+game = True
+
+
+
 def main():
-    global champ
-    board_print()
-    player_move()
-
-
-
-def player_move():
-    for i in range(5):
-        player = "X"
-        move(player)
+    while game:
+        print_board(board)
+        player(board)
         winner()
-        if champ != "X" and i < 4 :
+        playerchange()
 
-            for j in range(3):
-                player = "O"
-                move(player)
-                winner()
+def print_board(board):
+    print (board[0] + "," + board[1] + "," + board[2])
+    print (board[3] + "," + board[4] + "," + board[5])
+    print (board[6] + "," + board[7] + "," + board[8]) 
 
-                if winner == "O":
-                    print("Congratulations")
-                    print()
-                    break
-        elif champ == "X":
-            print("Congratulations")
-            print()
-            break
+def player(board):
+    choose = int(input( f"{playerone}'s turn to choose a square (1-9): ")) 
+    if choose >= 1 and choose <= 9:
+        board[choose-1] = playerone
 
-def move(player):
-    position = int(input("choose a square (1-9): "))
-    position -= 1
-    board[position] = player
-    board_print()
+def playerchange():
+    global playerone
+    if playerone == "X":
+        playerone = "O"
+    else: playerone = "X"
+
+
+def winnerhorizont(board):
+    global champ
+    if board[0] ==  board[1] == board[2]:
+        champ = board[0]
+        return True
+    elif board[3] ==  board[4] == board[5]:
+        champ = board[3]
+        return True
+    elif board[6] ==  board[7] == board[8]:
+        champ = board[6]
+        return True
+
+def winnercolum(board):
+    global champ
+    if board[0] ==  board[3] == board[6]:
+        champ = board[0]
+        return True
+    elif board[1] ==  board[4] == board[7]:
+        champ = board[1]
+        return True
+
+    elif board[2] ==  board[5] == board[8]:
+        champ = board[2]
+        return True
+
+def winnerdiag(board):
+    global champ
+    if board[0] ==  board[4] == board[8]:
+        champ = board[0]
+        return True
+    elif board[2] ==  board[4] == board[6]:
+        champ = board[2]
+        return True
 
 def winner():
-    global champ
-    line()
-    vertical()
-    diagonal()
-    
-def line():
-    global champ
-    if board[0]== board[1]==board[2] !="1":
-        champ = board[0]
-    elif board[3] ==  board[4] == board[5] != "1":
-        champ = board[3]
-    elif board[6] ==  board[7] == board[8] != "1":
-        champ = board[6]
-def vertical():
-    global champ
-    if board[0] ==  board[3] == board[6] != "1":
-        champ = board[0]
-    elif board[1] ==  board[4] == board[7] != "1":
-        champ = board[1]
-    elif board[2] ==  board[5] == board[8] != "1":
-        champ = board[2]
-def diagonal():
-    global champ
-    if board[0] ==  board[4] == board[8] != "-":
-        champ = board[0]
-    elif board[2] ==  board[4] == board[6] != "-":
-        champ = board[2]
-
-def board_print():
-    print()
-    print(f"{board[0]}|{board[1]}|{board[2]}")
-    print('-----')
-    print(f"{board[3]}|{board[4]}|{board[5]}")
-    print('-----')
-    print(f"{board[6]}|{board[7]}|{board[8]}")
-    print()
-
-
+    global game
+    if winnercolum(board) or winnerdiag(board) or winnerhorizont(board):
+        print(f"Congratulation player {champ}")
+        game = False
 
 if __name__ == "__main__":
     main()
-
-
- 
-
-
-
-
-
-
